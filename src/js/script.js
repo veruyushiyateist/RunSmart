c360eefd1163f1dd471c6a37a1b462cdb3fd816d
lyubomirpavlick <38151441+veruyushiyateist@users.noreply.github.com>
2020-07-21 13:18:18 +0300

    slider

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+      });         
+}); 
