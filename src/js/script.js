899c2e25a63199124cc1b78a8d25689f831d87f7
Lyubomir <lybomir.pavlick@gmail.com>
2020-07-22 16:26:49 +0300

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,3 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         speed: 1200,
-//         adaptiveHeight: true,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/left.png"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right.png"></button>',
-//         responsive: [
-//           {
-//             breakpoint: 992,
-//             settings: {
-//               dots: true,
-//               arrows: false
-//             }
-//           }
-//         ]
-//       });         
-// }); 
-
 const slider = tns({
   container: '.carousel__inner',
   items: 1,
@@ -31,4 +13,13 @@ document.querySelector('.prev').addEventListener('click', function () {
 
 document.querySelector('.next').addEventListener('click', function () {
   slider.goTo('next');
+});
+
+$(document).ready(function(){
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.tabs__content').removeClass('active').eq($(this)
+      .index()).addClass('active');
+  });
 });
