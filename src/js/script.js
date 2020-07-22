d0f17a3be98a5015adf6995e36f248340d9c8828
lyubomirpavlick <38151441+veruyushiyateist@users.noreply.github.com>
2020-07-22 12:56:15 +0300

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,34 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: true
-      });         
-}); 
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/left.png"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right.png"></button>',
+//         responsive: [
+//           {
+//             breakpoint: 992,
+//             settings: {
+//               dots: true,
+//               arrows: false
+//             }
+//           }
+//         ]
+//       });         
+// }); 
+
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  nav: false
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
+});
